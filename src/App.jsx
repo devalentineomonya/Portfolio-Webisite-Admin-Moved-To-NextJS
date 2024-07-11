3db394dcbc28d92210c0d65928a00c3e8d935ce8
Valentine Omonya <valomosh254@gmail.com>
2024-07-11 22:41:01 +0300

    Added breadcrumd

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,26 @@
-import { Button, Layout } from "antd";
+import { Breadcrumb, Button, Layout } from "antd";
 import Sider from "antd/es/layout/Sider";
 import { Content, Footer, Header } from "antd/es/layout/layout";
 import "./App.css";
 import AsideNavigation from "./components/AsideNavigation/AsideNavigation";
 
 function App() {
+  const breadcrumbItems = [
+    {
+      title: "Home",
+    },
+    {
+      title: "Application Center",
+      href: "",
+    },
+    {
+      title: "Application List",
+      href: "",
+    },
+    {
+      title: "An Application",
+    },
+  ];
   return (
     <Layout>
       <Header className="header">header</Header>
@@ -17,7 +33,16 @@ function App() {
         >
           <AsideNavigation />
         </Sider>
-        <Content>main content</Content>
+
+        <Content>
+          <Header className="main-content-header">
+            <div className="main-content-title">
+              Dashboard
+            </div>
+            <Breadcrumb separator=">" items={breadcrumbItems} className="main-content-breadcrumb" />
+          </Header>
+          main content
+        </Content>
       </Layout>
     </Layout>
   );
--- src/components/AsideNavigation/AsideNavigation.jsx
+++ src/components/AsideNavigation/AsideNavigation.jsx
@@ -1,11 +1,17 @@
-import React from 'react'
+import Title from "antd/es/typography/Title";
+import React from "react";
+import "./asidenavigation.css";
+import { Divider } from "antd";
 
 const AsideNavigation = () => {
   return (
     <div>
-      
+      <Title type="success" level={5} className="aside-title">
+        Devalentine Admin Dashboard
+      </Title>
+      <Divider className="aside-title-separator" />
     </div>
-  )
-}
+  );
+};
 
-export default AsideNavigation
+export default AsideNavigation;
